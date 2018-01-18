8f95b920591d8ee3652386cfe630c20d744325fd
vincentotsai <totsai@139.com>
2018-01-19 01:27:41 +0800

    add: douban

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ let func = () => {
 }
 func()
 
-$('#app').text('Hello parcel.').css('color','white')
+$('#app').text('Hello parcel.').css('color', 'white')
+
+$('#btn').on('click', loadDate)
+
+function loadDate() {
+  log('clicked...')
+  $('#status').text('Loading...')
+  $.ajax({
+    method: 'GET',
+    dataType: "jsonp",
+    url: 'https://api.douban.com/v2/movie/top250?count=10'
+  }).done(function (data) {
+    console.log('data->', data)
+    for (var i = 0; i < data.subjects.length; i++) {
+      var ele = data.subjects[i];
+      var img = document.createElement('img')
+      img.src = ele.images.small
+      $('#status').text('loaded!')
+      $('#douban250').append(img)
+    }
+  })
+}
