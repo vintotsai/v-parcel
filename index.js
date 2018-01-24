8de27293815bfc3f0ef79d114bff565b02314985
vincentotsai <caiwc@tsingning.com>
2018-01-24 13:56:54 +0800

    update

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 import './reset.less'
 import './main.less'
 
-let func = () => {
-  console.log('array function..')
-}
-func()
-
 $('#btn').on('click', loadDate)
 
 function loadDate() {
   $('#status').text('Loading...')
   $.ajax({
     method: 'GET',
-    dataType: "jsonp",
+    dataType: 'jsonp',
     url: 'https://api.douban.com/v2/movie/top250?count=10'
-  }).done(function (data) {
+  }).done(function(data) {
     console.log('data->', data)
 
     for (let i = 0; i < data.subjects.length; i++) {
-      let ele = data.subjects[i];
+      let ele = data.subjects[i]
       let img = document.createElement('img')
       img.src = ele.images.small
-      $('#status').text('loaded!')
       $('#douban250').append(img)
     }
+    $('#status').text('loaded!')
   })
 }
